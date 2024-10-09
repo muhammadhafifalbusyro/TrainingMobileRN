150456bf79c3793102b3ca514e666a46d7e9e451
Muhammad Hafif Al Busyro <muhammadhafifalbusyro@gmail.com>
2024-10-09 10:20:53 +0700

    navigation stack

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
 import {AppRegistry} from 'react-native';
 import 'react-native-gesture-handler';
 
-import App from './App';
+// import App from './App';
 
-// import App from './src/App';
+import App from './src/App';
 
 import {name as appName} from './app.json';
 
--- src/App.js
+++ src/App.js
@@ -1,8 +1,108 @@
-import React from 'react'
-import StackNavigator from './routes/StackNavigator'
+// import React from 'react'
+// import StackNavigator from './routes/StackNavigator'
 
-const App = () => {
-    return <StackNavigator/>
+// const App = () => {
+//     return <StackNavigator/>
+// }
+
+// export default App
+
+
+// In App.js in a new project
+
+// import * as React from 'react';
+// import { View, Text, Button, Alert } from 'react-native';
+// import { NavigationContainer, useNavigation } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// function HomeScreen({navigation}) {
+
+//     // const navigation = useNavigation()
+
+//     const handlerNavigate = () => {
+//         // Alert.alert("Hallo")
+//         // navigation.navigate('Setting')
+//         navigation.navigate('Setting')
+//     }
+
+//     return (
+//         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text >Home Screen</Text>
+//         <Button title='Go To Setting' onPress={()=>handlerNavigate()}/>
+//         </View>
+//     );
+// }
+// function SettingScreen() {
+//     return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Setting Screen</Text>
+//       </View>
+//     );
+//   }
+
+// // Inisialiasi Stack Navigatornya
+// const Stack = createNativeStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//         <Stack.Navigator 
+//         initialRouteName='Home'
+//         >
+//             <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home Screen' }}/>
+//             <Stack.Screen name='Setting' component={SettingScreen} />
+//         </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default App;
+
+
+import * as React from 'react';
+import { View, Text, Button, Alert } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+
+    // const navigation = useNavigation()
+
+    const handlerNavigate = () => {
+        // Alert.alert("Hallo")
+        // navigation.navigate('Setting')
+        navigation.navigate('Setting')
+    }
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text >Home Screen</Text>
+        <Button title='Go To Setting' onPress={()=>handlerNavigate()}/>
+        </View>
+    );
+}
+function SettingScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Setting Screen</Text>
+      </View>
+    );
+  }
+
+// Inisialiasi Stack Navigatornya
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator 
+        initialRouteName='Home'
+        >
+            <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home Screen' }}/>
+            <Stack.Screen name='Setting' component={SettingScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-export default App
+export default App;
